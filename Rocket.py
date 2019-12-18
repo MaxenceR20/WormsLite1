493ff01a20d4b5ca2914536b0ef908781479a9df
MaxenceR20 <59018906+MaxenceR20@users.noreply.github.com>
2019-12-18 16:20:58 +0100

    Add files via upload

--- Rocket.py
+++ Rocket.py
@@ -3,11 +3,21 @@ from bazooka import *
 
 
 class Rocket(object):
-    def __init__(self,parent,x,y,coul):
-        self.parent=parent
-        self.x, self.y = x, y
-        self.r=self.parent.create_oval(self.x-5,self.y-5,self.x+5,self.y+5,fill=coul)
+    
+    def __init__(self,master,x,y,coul):
+        self.master=master
+        self.x=x
+        self.y=y
+        self.r=self.master.create_oval(self.x-5,self.y-5,self.x+5,self.y+5,fill=coul)
+    def destroy(self):
+        self.master.delete(self.r)
+        print("destruction")
 
+    def envoie(self):
+            global dx, dy
+            self.master.move(balle1,dx,dy)
+            #On repete cette fonction
+            tk.after(20,deplacement)
 
 
 
--- bazooka.py
+++ bazooka.py
@@ -3,8 +3,8 @@ from math import pi, sin, cos
 
 class Bazooka(object):
     """Petit canon graphique"""
-    def __init__(self, boss, x, y):
-        self.boss = boss
+    def __init__(self, master, x, y):
+        self.master = master
         # référence du canevas
         self.x1, self.y1 = x, y
         # axe de rotation du canon
@@ -12,10 +12,11 @@ class Bazooka(object):
         self.lbu = 50
         # longueur de la buse
         self.x2, self.y2 = x + self.lbu, y
-        self.buse = boss.create_line(self.x1, self.y1, self.x2, self.y2,width =10)
+        self.buse = master.create_line(self.x1, self.y1, self.x2, self.y2,width =10)
         # dessiner ensuite le corps du canon par-dessus :
         r = 15                     # rayon du cercle
-        boss.create_oval(x-r, y-r, x+r, y+r, fill='blue', width =3)
+        master.create_oval(x-r, y-r, x+r, y+r, fill='blue', width =3)
+       
 
     def orienter(self, angle):
             "choisir l'angle de tir du canon"
@@ -24,7 +25,7 @@ class Bazooka(object):
             self.angle = float(angle)*2*pi/360
             self.x2 = self.x1 + self.lbu*cos(self.angle)
             self.y2 = self.y1 - self.lbu*sin(self.angle)
-            self.boss.coords(self.buse, self.x1, self.y1, self.x2, self.y2)
+            self.master.coords(self.buse, self.x1, self.y1, self.x2, self.y2)
         
 
 
